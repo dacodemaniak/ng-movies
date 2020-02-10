c5d573566fc7f3d42d4593f674ecbf1b0580e0b3
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-02-10 11:42:13 +0100

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'movies';
+  public title: string = 'movies';
+
+  public defaultCountry: string = 'us';
+
+  public movies: any[] = [
+    {
+      title: 'Joker',
+      year: 2019,
+      country: 'us'
+    },
+    {
+      title: 'Avengers',
+      year: 2018,
+      country: 'us'
+    },
+    {
+      title: 'Il était une fois dans l\'ouest',
+      year: 1975,
+      country: 'it'
+    }
+  ];
+
+  public toggleCountry(): void {
+    if (this.defaultCountry == 'us') {
+      this.defaultCountry = 'it';
+    } else {
+      this.defaultCountry = 'us';
+    }
+  }
 }
