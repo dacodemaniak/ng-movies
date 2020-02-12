418912bd64673db248f5001bb4ad86bf49325d8f
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-02-12 13:54:10 +0100

    Cosmetics on search component

--- src/app/pages/home/search/search.component.ts
+++ src/app/pages/home/search/search.component.ts
@@ -14,6 +14,7 @@ export class SearchComponent implements OnInit {
   public searchTerm: string;
 
   @Output() movies: EventEmitter<Movie[]> = new EventEmitter<Movie[]>();
+  
   constructor(private movieService: MovieService) { }
   
 
