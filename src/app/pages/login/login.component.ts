e6731d4a921a8b74f4b0bf05dc70f7a48af1c018
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-02-13 14:35:39 +0100

    Add controls to form and mat-error component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
 
-  constructor() { }
 
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  public get login(): AbstractControl {
+    return this.loginForm.controls.login;
+  }
+
+  public get password(): AbstractControl {
+    return this.loginForm.controls.password;
+  }
+  
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      login: [
+        '',
+        Validators.compose(
+          [Validators.required, Validators.minLength(5)]
+        )
+      ],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8)
+        ])
+      ]
+    });
   }
 
 }
