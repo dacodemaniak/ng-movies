a52afed3f483293b16c9f79bc71803ec408533a4
Jean-Luc Aubert <jean-luc.a@web-projet.com>
2020-03-03 12:22:16 +0100

    Load translations at startup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
+import { TranslationService } from './core/services/translation.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { NgModule, APP_INITIALIZER, Injector } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,7 +13,7 @@ import { UiModule } from './shared/ui/ui.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
 
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import { SearchComponent } from './pages/home/search/search.component';
 
 import { AppConfig } from './core/init/app-config';
@@ -24,6 +25,28 @@ import { TokenInterceptorService } from './core/services/token-interceptor.servi
 import { LOCATION_INITIALIZED } from '@angular/common';
 // Translate module
 import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+// Define a function that invoke TranslationService
+export function translationInitializerFactory(
+  translateService: TranslateService, // The one from @ngx-translate
+  translationService: TranslationService, // The one of our own... will be instanciate
+  injector: Injector // Injection service
+) {
+  return (): Promise<void> => {
+    return translationService.init(translateService, injector);
+  }; // Returns a function that returns a Promise
+}
+
+// HttpLoaderFactory to get translation files
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(
+    http,
+    './assets/i18n/',
+    '.json'
+  );
+}
 
 export function initializeApp(appConfig: AppConfig) {
   return (): Promise<any> => {
@@ -46,12 +69,29 @@ export function initializeApp(appConfig: AppConfig) {
     UiModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     AppConfig,
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true},
-    { provide: APP_INITIALIZER,useFactory: initializeApp, deps: [AppConfig], multi: true}
+    { provide: APP_INITIALIZER, useFactory: initializeApp, deps: [AppConfig], multi: true},
+    {
+      provide: APP_INITIALIZER, // Token Angular fourni lors de l'initialisation de l'application
+      useFactory: translationInitializerFactory, // Usine à utiliser... (notre fonction tordue)
+      deps: [
+        TranslateService,
+        TranslationService,
+        Injector
+      ], // Dépendances du service lui même
+      multi: true // Sinon, les autres fournisseurs ne pourraient pas être chargés...
+    }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/core/services/translation.service.ts
+++ src/app/core/services/translation.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { LOCATION_INITIALIZED } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
+import { Injectable, Injector } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TranslationService {
 
+  private _language: string;
+
   constructor() { }
+
+  public init(
+    translateService: TranslateService,
+    injector: Injector
+  ): Promise<void> {
+    return new Promise<void>((resolve: any) => {
+      // Get the LOCATION_INITIALIZER token
+      injector.get(LOCATION_INITIALIZED, Promise.resolve(null)).then(() => {
+        // Promise taken... so... let's get the current language
+        const navigatorLanguage: string = window.navigator.language;
+        const userLanguage: string = navigatorLanguage.split('-')[0];
+
+        // Check for userLanguage against our known languages and fallback to 'en' if no matching
+        this._language = /(fr|en)/gi.test(userLanguage) ? userLanguage : 'en';
+
+        // We can now load translations... using TranslateService param
+        translateService
+          .use(this._language) // Tell the service to use the current language as Observable
+          .subscribe(() => {
+            console.log(`Translations loaded from ${this._language}`);
+            resolve(null); // Promise must be taken
+          });
+      });
+    });
+  }
 }
